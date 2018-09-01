a8f205f226ec6f020ade3247ca4a945ee6f50b96
Diego Biazin <diego_biazin@hotmail.com>
2018-09-01 12:25:14 -0300

    Implementando filtro

--- app/src/main/java/com/diegobiazin/motivation/view/MainActivity.kt
+++ app/src/main/java/com/diegobiazin/motivation/view/MainActivity.kt
@@ -2,12 +2,64 @@ package com.diegobiazin.motivation.view
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.diegobiazin.motivation.R
+import com.diegobiazin.motivation.util.MotivationConstants
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private var mFilter: Int = MotivationConstants.PHRASE_FILTER.ALL
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Eventos
+        setListeners()
+
+        //inicializa
+        handleFilter(R.id.imageAll)
     }
+
+    override fun onClick(view: View) {
+        val id = view.id
+
+        val listId = listOf(R.id.imageAll, R.id.imageSun, R.id.imageHappy)
+        if (id in listId) {
+            handleFilter(id)
+        } else {
+            refreshPhrase()
+        }
+    }
+
+    private fun setListeners() {
+        imageAll.setOnClickListener(this)
+        imageSun.setOnClickListener(this)
+        imageHappy.setOnClickListener(this)
+        buttonNewPhrase.setOnClickListener(this)
+    }
+
+    private fun handleFilter(id: Int) {
+
+        imageAll.setImageResource(R.drawable.ic_all_unselected)
+        imageSun.setImageResource(R.drawable.ic_sun_unselected)
+        imageHappy.setImageResource(R.drawable.ic_happy_unselected)
+
+        if (id == R.id.imageAll) {
+            mFilter = MotivationConstants.PHRASE_FILTER.ALL
+            imageAll.setImageResource(R.drawable.ic_all_selected)
+        } else if (id == R.id.imageSun) {
+            mFilter = MotivationConstants.PHRASE_FILTER.SUN
+            imageSun.setImageResource(R.drawable.ic_sun_selected)
+        } else {
+            mFilter = MotivationConstants.PHRASE_FILTER.HAPPY
+            imageHappy.setImageResource(R.drawable.ic_happy_selected)
+        }
+    }
+
+    private fun refreshPhrase() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
 }
