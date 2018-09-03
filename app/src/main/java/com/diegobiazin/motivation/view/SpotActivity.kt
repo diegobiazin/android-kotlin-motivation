30f0dd9c45f68b39146784b58239fbc8e46c06df
Diego Biazin <diego_biazin@hotmail.com>
2018-09-02 21:36:17 -0300

    Utilizando ConstraintLayout

---
+++ app/src/main/java/com/diegobiazin/motivation/view/SpotActivity.kt
@@ -0,0 +1,12 @@
+package com.diegobiazin.motivation.view
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SpotActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_spot)
+    }
+}
